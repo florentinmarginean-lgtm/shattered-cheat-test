0c40566b00cf76ef4a88b22d3c9693e837b44f7c
florentinmarginean-lgtm <florentinmarginean@gmail.com>
2026-09-10 21:22:15 +0300

    Update Level.java

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/levels/Level.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/levels/Level.java
@@ -1012,7 +1012,7 @@ public abstract class Level implements Bundlable {
 
 		}
 		// --- SPAWN +20 WAND WITH RETURN CRYSTAL ---
-		if (item != null && item.getClass().getSimpleName().contains("ReturnCrystal")) {
+		if (item != null && item.getClass().getSimpleName().contains("EscapeCrystal")) {
 			com.shatteredpixel.shatteredpixeldungeon.items.wands.WandOfDisintegration wand = 
 				new com.shatteredpixel.shatteredpixeldungeon.items.wands.WandOfDisintegration();
 			wand.upgrade(20);
