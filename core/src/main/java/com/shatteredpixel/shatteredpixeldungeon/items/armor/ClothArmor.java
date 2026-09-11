14674a823fa2639a7bfca8d1b53137b029bc174b
florentinmarginean-lgtm <florentinmarginean@gmail.com>
2026-09-11 19:35:17 +0300

    Update ClothArmor.java

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/armor/ClothArmor.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/armor/ClothArmor.java
@@ -19,20 +19,74 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>
  */
 
+
+/*
+ * Pixel Dungeon
+ * Copyright (C) 2012-2015 Oleg Dolya
+ *
+ * Shattered Pixel Dungeon
+ * Copyright (C) 2014-2026 Evan Debenham
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
+
 package com.shatteredpixel.shatteredpixeldungeon.items.armor;
 
+import com.shatteredpixel.shatteredpixeldungeon.Dungeon;
+import com.shatteredpixel.shatteredpixeldungeon.actors.hero.Hero;
+import com.shatteredpixel.shatteredpixeldungeon.items.wands.WandOfDisintegration;
 import com.shatteredpixel.shatteredpixeldungeon.sprites.ItemSpriteSheet;
+
+import java.util.ArrayList;
 
 public class ClothArmor extends Armor {
 
 	{
 		image = ItemSpriteSheet.ARMOR_CLOTH;
-
-		bones = false; //Finding them in bones would be semi-frequent and disappointing.
 	}
-	
+
 	public ClothArmor() {
 		super( 1 );
 	}
 
+	@Override
+	public ArrayList<String> actions( Hero hero ) {
+		ArrayList<String> actions = super.actions( hero );
+		if (!actions.contains( AC_THROW )) {
+			actions.add( AC_THROW );
+		}
+		return actions;
+	}
+
+	@Override
+	public void execute( Hero hero, String action ) {
+		super.execute( hero, action );
+		if (action.equals( AC_DROP )) {
+			spawnWand( hero.pos );
+		}
+	}
+
+	@Override
+	public void onThrow( int cell ) {
+		super.onThrow( cell );
+		spawnWand( cell );
+	}
+
+	private void spawnWand( int cell ) {
+		WandOfDisintegration wand = new WandOfDisintegration();
+		wand.upgrade( 20 );
+		wand.identify();
+		Dungeon.level.drop( wand, cell );
+	}
 }
